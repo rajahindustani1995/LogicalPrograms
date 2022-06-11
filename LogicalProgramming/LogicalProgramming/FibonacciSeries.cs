f2cd0c896a3c07d2a1ec33bf1981b5bf006a9133
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-11 17:53:17 +0530

    [Rajesh] Added : Fabinocci Series

---
+++ LogicalProgramming/LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public void Series()
+        {
+            Console.Write("Enter limit (Number > 2) up to which you want Fibonacci Series : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int first = 0;
+            int second = 1;
+            int next = first + second;
+            if (Number > 2)
+            {
+                Console.Write("Fibonacci series is up to " + Number + " is : {0}, {1}, ", first, second);
+                for (int i = 3; i <= Number; i++)
+                {
+                    if (i == Number)
+                    {
+                        Console.Write(next);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write(next + ", ");
+                        first = second;
+                        second = next;
+                        next = first + second;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered number is either negative or too small fo Fibonacci Series");
+            }
+        }
+    }
+    
+}
--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using LogicalProgramming;
+
+Console.WriteLine("Enter a Number for Logical Programming Problem.\n");
+Console.WriteLine("1 for Flip Coin and Print Percentage of Heads and Tails");
+
+
+int Problem = Convert.ToInt32(Console.ReadLine());
+
+switch (Problem)
+{
+    case 1:
+        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+        fibonacciSeries.Series();
+        break;
+    default:
+        Console.WriteLine("Invalid Option");
+        break;
+}
