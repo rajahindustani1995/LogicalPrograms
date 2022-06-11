0d05bcbad5c11da6624903fd9472d2d2c33662b2
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-11 18:26:48 +0530

    [Rajesh] Added : Perfect Number

---
+++ LogicalProgramming/LogicalProgramming/PerfectNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PerfectNumber
+    {
+        public void Perfect()
+        {
+            Console.Write("Enter the number for Perfect Number calculation : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            //Divisors calculation logic
+            Console.Write("Divisors are : ");
+            for (int i = 1; i <= (Number / 2); i++)
+            {
+                if (Number % i == 0)
+                {
+                    Console.Write(i + " ");
+                    result += i;
+                }
+            }
+            Console.WriteLine("\nAddition of Divisors is : " + result);
+            if (result == Number)
+            {
+                Console.WriteLine("The Entered Number is a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("The Entered Number is not a Perfect Number");
+            }
+
+        }
+    }
+}
--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,8 +1,8 @@
 ﻿using LogicalProgramming;
 
 Console.WriteLine("Enter a Number for Logical Programming Problem.\n");
-Console.WriteLine("1 for Flip Coin and Print Percentage of Heads and Tails");
-
+Console.WriteLine("1 for Fibonacci Series");
+Console.WriteLine("2 for Perfect Number");
 
 int Problem = Convert.ToInt32(Console.ReadLine());
 
@@ -11,6 +11,10 @@ switch (Problem)
     case 1:
         FibonacciSeries fibonacciSeries = new FibonacciSeries();
         fibonacciSeries.Series();
+        break;
+    case 2:
+        PerfectNumber perfectNumber = new PerfectNumber();
+        perfectNumber.Perfect();
         break;
     default:
         Console.WriteLine("Invalid Option");
