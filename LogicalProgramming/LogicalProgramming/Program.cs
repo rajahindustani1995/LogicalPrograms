e9682a2de3802cdc91e856d20fd1ca3109250c76
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-11 18:55:20 +0530

    [Rajesh] Added : Reverse Number

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Enter a Number for Logical Programming Problem.\n");
 Console.WriteLine("1 for Fibonacci Series");
 Console.WriteLine("2 for Perfect Number");
 Console.WriteLine("3 for Prime Number");
+Console.WriteLine("4 for Reverse Number");
 
 int Problem = Convert.ToInt32(Console.ReadLine());
 
@@ -20,6 +21,10 @@ switch (Problem)
     case 3:
         PrimeNumber primeNumber = new PrimeNumber();
         primeNumber.Prime();
+        break;
+    case 4:
+        ReverseNumber reverseNumber = new ReverseNumber();  
+        reverseNumber.Reverse();
         break;
     default:
         Console.WriteLine("Invalid Option");
---
+++ LogicalProgramming/LogicalProgramming/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.Write("Enter the Number to Revers : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+
+            while (number != 0)
+            {
+                reverse *= 10;
+                reverse += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine("Reversed Number is : " + reverse);
+        }
+    }
+}
